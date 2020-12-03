8fda7b3840713acd81925401bb2467ea31c9b63d
LeslieDaniela <33047756+LeslieDaniela@users.noreply.github.com>
2020-12-03 00:44:48 -0700

    Update shell.py

--- shell/shell.py
+++ shell/shell.py
@@ -1,186 +1,166 @@
-#!/usr/bin/env python3
+#! /usr/bin/env python3
 
-import os, sys, re
+import sys, os, re
 
-def setPrompt():
+def main():
+  
     while True:
-        defaultPrompt = os.getcwd() + "$"
-        
-        # PS1 variable
+
+        # Prompt String 1
         if 'PS1' in os.environ:
-            defaultPrompt = os.environ['PS1']
+            os.write(1, (os.environ['PS1']).encode())
+        
+        # Default Prompt
+        else:
+            os.write(1, ("$").encode())
 
-            try:
-                # Writes bytestring 1 to file descriptor bytes 
-                os.write(1, defaultPrompt.encode())
-                # Reads n bytes from associated file descriptor
-                data = os.read(0, 10000)
+        args = os.read(0, 1024)
 
-                if len(data) == 0:
-                    break
-                data  = data.decode().split("\n")
+        if len(args) == 0:
+            break
 
-                for information in data:
-                    shellCommands(information.split())
-            # Error handling
-            except EOFError:
-                sys.exit(1)
+        args = args.decode().split("\n")
+
+        if not args: 
+            continue
+        
+        for arg in args:
+            shellCommands(arg.split())
 
 # Shell Commands Method
-def shellCommands(data):
-    # Checks if Empty
-    if len(data) == 0:
-        return
-    # Exit Command 
-    if data[0].lower() == 'exit':
-        sys.exit(0)
-        
-    # Directory Command
-    if data[0].lower() == 'cd':
+def shellCommands(args):
 
-        # chdir for Navigating Directories
-        try:
-            os.chdir(data[1])
+    # Checks if Empty
+    if len(args) == 0:
+        return
+
+    # Exit Command 
+    elif args[0].lower == "exit":
+        sys.exit(0)
+
+    # Directory Command
+    elif args[0] == "cd":
+        
+        try: 
+            # chdir for Navigating Directories
+            os.chdir(args[1])
+        
+        # cd <no directory> : Return to Parent Directory 
         except IndexError:
             os.write(2, ("Include Target Directory \n").encode())
-        except FileNotFoundError:
-            os.write(2, ("No such file or directory \n").encode())
-        
-    elif '|' in data:
-        # Initialize fork
-        forking = os.fork()
-        
-        # Child Process
-        if forking == 0:
-            simplePipes(data)
-        elif forking < 0:
-            os.write(2, ("Failed to Fork \n").encode())
-            sys.exit(1)
 
-        # Parent Process
-        else:
-            if data[-1] != "&":
-                state = os.wait()
-                if state[1] != 0 and state[1] != 256:
-                    os.write(2, ("Program Terminated with exit code: %d\n" % state[1].encode()))
+        # Directory Not Found 
+        except FileNotFoundError:
+            os.write(1, ("cd: %s: No such file or directory \n" % args[1]).encode())
+            pass
+
+    elif "|" in args:  
+        pipe(args)
 
     else:
-        # Initialize fork
-        forking = os.fork()
+        rc = os.fork()
+        wait = True
 
-        # Boolean for wait
-        waitingProcess = True
+        if "&" in args: 
+            args.remove("&")
+            wait = False
 
-        if "&" in data:
-            waitingProcess = False
-            data.remove("&")
-
-        if forking == 0:
-            redirectInputOutput(data)
-        elif forking < 0 :
-            os.write(2, ("Failed to Fork \n").encode())
+        if rc < 0: 
+            os.write(2, ("Failed to Fork, returning... %d\n" % rc).encode())
             sys.exit(1)
+
+        elif rc == 0:
+            executeCommand(args)
+            sys.exit(0)
 
         else:
-            if waitingProcess:
-                state = os.wait()
-                if state[1] !=0 and state[1] !=256:
-                     os.write(2, ("Program Terminated with exit code: %d\n" % state[1].encode()))
+            if wait:
+                childpid = os.wait()
 
-def simplePipes(data):
-    # pipline for converters from one file to another
-    readPipe, writePipe = os.pipe()
-    read = data[data.index("|")+1]
-    write = data[0:data.index("|")]
 
-    # Parent Process
-    if forking > 0:
-        # Closes DATA file descriptor
-        os.close(0)
-        # Duplicates pipe reader to file descriptor 0
-        os.dup2(readPipe, 0)
+def pipe(args):
+    left = args[0:args.index("|")]
+    right = args[args.index("|") + 1:]
 
-        for fileDescriptor in (readPipe, writePipe): 
-           os.close(fileDescriptor)
-        if "|" in read:
-            simplePipes(read)
+    pr, pw = os.pipe()
+    rc = os.fork()
 
-        # Calls method to run process
-        redirectInputOutput(read)
-        os.write(2, ("Not executable\n").encode())
+    if rc < 0:
+        os.write(2, ("Failed to Fork, returning... %d\n" % rc).encode())
         sys.exit(1)
-
-    # Child Process
-    elif forking == 0:
-        # Closes OUTPUT file descriptor
+     
+    elif rc == 0: 
         os.close(1)
-        # Duplicates pipe writer to file descriptor 1
-        os.dup2(writePipe, 1)
-
-        for fileDescriptor in (readPipe, writePipe): 
-            # Closes pipe read and write
-            os.close(fileDescriptor)
-
-        # Calls method to run process
-        redirectInputOutput(write)
-        os.write(2,("Not executable\n").encode())
+        os.dup(pw)
+        os.set_inheritable(1, True)
+        for fd in (pr, pw):
+            os.close(fd)
+        executeCommand(left)
+        os.write(2, ("Could not exec %s\n" % left[0]).encode())
+        sys.exit(1)
+        
+    else:
+        os.close(0)
+        os.dup(pr)
+        os.set_inheritable(0, True)
+        for fd in (pw, pr):
+            os.close(fd)
+        
+    
+        if "|" in right:
+            pipe(right)
+        
+        executeCommand(right)
+        os.write(2, ("Could not exec %s\n" % right[0]).encode())
         sys.exit(1)
 
-    # Fork Fails
+
+def redirectInputOutput(args):
+    
+    
+    if '>' in args: 
+        os.close(1)
+        os.open(args[args.index('>')+1], os.O_CREAT | os.O_WRONLY)
+        os.set_inheritable(1,True)
+        args.remove(args[args.index('>') + 1])
+        args.remove('>')
+        
     else: 
-        os.write(2,("Failed to Fork \n").encode())
-        sys.exit(1)
- 
-def redirectInputOutput(data):
-    try:
-        # INPUT 
-        if '<' in data: 
-            os.close(0)
-            # Open file to read
-            os.open(data[data.index('<') +1], os.O_RDONLY)
-            # Set inheritable flag of standard output file descriptor to 0 
-            os.set_inheritable(0, True)
-            # Removes file directory name
-            data.remove(data[data.index('<')+1])
-            # Removes <
-            data.remove('<')
-
-        # OUTPUT
-        if '>' in data: 
-            os.close(1)
-            # Creates file to write
-            os.open(data[data.index('>') +1], os.O_CREAT | os.O_WRONLY)
-            # Set inheritable flag of standard output file descriptor to 1
-            os.set_inheritable(1,True)
-            # Removes file directory name
-            data.remove(data[data.index('>')+1])
-            # Removes >
-            data.remove('>') 
-
-    except IndexError:
-        os.write(2, "Not able to redirect\n".encode())
-
-    try:
-        if data[0][0] == '/':
-            # Child process uses execve to run command
-            os.execve(data[0], data, os.environ) 
-    except FileNotFoundError:
-        pass
-    except Exception:
-        sys.exit(1)
-
-    # Absolute path not specified uses environ $PATH
-    for dir in re.split(":", os.environ['$PATH']): 
-        targetDirectory  = "%s/%s" % (dir, data[0])
+        os.close(0)
+        os.open(args[args.index('<')+1], os.O_RDONLY);
+        os.set_inheritable(0, True)
+        args.remove(args[args.index('<') + 1])
+        args.remove('<')
+    
+    for dir in re.split(":", os.environ['PATH']): 
+        prog = "%s/%s" % (dir,args[0])
         try:
-            # Attempts execution
-            os.execve(targetDirectory, data, os.environ)
+            os.execve(prog, args, os.environ) 
         except FileNotFoundError:
             pass
-        except Exception:
-            sys.exit(1)
-            
-    os.write(2, ("Command not found\n").encode())
-    sys.exit(1)
-    
-setPrompt()
+
+    os.write(2, ("%s: command not found\n" % args[0]).encode())
+    sys.exit(0)
+        
+def executeCommand(args):
+    if "/" in args[0]:
+        program = args[0]
+        try:
+            os.execve(program, args, os.environ)
+        except FileNotFoundError:  
+            pass 
+    elif ">" in args or "<" in args:
+        redirectInputOutput(args)
+    else:
+        for dir in re.split(":", os.environ['PATH']): 
+            program = "%s/%s" % (dir, args[0])
+            try:
+                os.execve(program, args, os.environ)
+            except FileNotFoundError:
+                pass
+                
+    os.write(2, ("%s: command not found\n" % args[0]).encode()) 
+    sys.exit(0)
+
+if __name__ == "__main__":
+    main()
